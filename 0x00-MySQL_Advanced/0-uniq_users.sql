4ea4c892a6cff958907b5d7a090d8ae0999ec5f3
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2024-08-07 20:47:58 +0100

    Advanced SQL

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,13 @@
+-- This script creates a table 'users' with the specified requirements
+
+-- Create the 'users' table if it does not already exist
+CREATE TABLE IF NOT EXISTS users (
+    -- The 'id' column: an integer that is never null, auto-increments, and is the primary key
+    id INT NOT NULL AUTO_INCREMENT,
+    -- The 'email' column: a string with a maximum length of 255 characters, never null and unique
+    email VARCHAR(255) NOT NULL UNIQUE,
+    -- The 'name' column: a string with a maximum length of 255 characters
+    name VARCHAR(255),
+    -- Set 'id' as the primary key of the table
+    PRIMARY KEY (id)
+);

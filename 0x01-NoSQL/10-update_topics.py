f8e48eef6e2fd7243486602cd3c33be9351fc443
Saheed Kehinde Yusuf <108232561+Da4midable@users.noreply.github.com>
2024-08-13 16:24:33 +0100

    changes all topics of a school document

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Module creates a function that changes all topics of a school document
+based on the name.
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """changes all topics of a school document based on the name"""
+    up_topic = mongo_collection.update_one(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
+    return up_topic.modified_count
